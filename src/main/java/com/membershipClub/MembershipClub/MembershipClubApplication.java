6ba1d339b78877a3a6a95b90bcd651e205206f6b
MariaLauraBelchior <marialaurabelchior007@gmail.com>
2025-05-07 21:18:32 -0300

    start of the project

---
+++ src/main/java/com/membershipClub/MembershipClub/MembershipClubApplication.java
@@ -0,0 +1,13 @@
+package com.membershipClub.MembershipClub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembershipClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/membershipClub/MembershipClub/MembershipClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.membershipClub.MembershipClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MembershipClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
